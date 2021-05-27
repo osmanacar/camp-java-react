f41d3b2b0516a687460448299dbacc60ad0b65b8
osman <osman@LAPTOP-37PRV6SD.home>
2021-05-27 15:06:18 +0300

    Java Camp Day 4

---
+++ javaCampDay4HomeworkCoffeeShop/src/Adapter/MernisServiceAdapter.java
@@ -0,0 +1,30 @@
+package Adapter;
+
+import Abstract.CustomerCheckService;
+import Entities.Customer;
+import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
+
+public class MernisServiceAdapter implements CustomerCheckService {
+
+	@Override
+	public boolean checkIfRealPerson(Customer customer) {
+
+		KPSPublicSoapProxy kpsPublicSoapProxy = new KPSPublicSoapProxy();
+
+		boolean proxyResult = false;
+
+		try {
+			proxyResult = kpsPublicSoapProxy.TCKimlikNoDogrula(
+													Long.parseLong(customer.nationalityId),
+													customer.firstName.toUpperCase(),
+													customer.lastName.toUpperCase(),
+													customer.dateOfBirth.getYear()
+																);
+		} catch (Exception e) {
+			System.out.println("Not a valid person");
+		}
+
+		return proxyResult;
+	}
+
+}
