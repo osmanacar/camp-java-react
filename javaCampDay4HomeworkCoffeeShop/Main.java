153b56e416252e1bbde9dd71c924250caa130f3b
osman <osman@LAPTOP-37PRV6SD.home>
2021-05-28 14:39:38 +0300

    Java Camp Day 4

--- javaCampDay4HomeworkCoffeeShop/Main.java
+++ javaCampDay4HomeworkCoffeeShop/Main.java
@@ -2,7 +2,6 @@ import java.time.LocalDate;
 
 import Abstract.CustomerManager;
 import Adapter.MernisServiceAdapter;
-import Concrete.NeroCustomerManager;
 import Concrete.StarbucksCustomerManager;
 import Entities.Customer;
 
@@ -12,6 +11,7 @@ public class Main {
 		// CustomerManager customerManager = new NeroCustomerManager();
 		CustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
 		customerManager.save(new Customer(1, "Pre", "Dart", LocalDate.of(1903, 1, 19), "19031903190"));
+
 	}
 
 }
